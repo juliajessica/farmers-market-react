c30927c777f744d6ff0b48839ca3e88a9721e647
julia sheremet <juliajessicasher@gmail.com>
2018-06-27 20:08:08 -0700

    Add a 404 component to and route it to a new page

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -5,6 +5,7 @@ import Home from './Home';
 import SeasonalProduceList from './SeasonalProduceList';
 import MarketScheduleList from './MarketScheduleList';
 import About from './About';
+import Error404 from './Error404';
 
 function Router(){
   return(
@@ -14,6 +15,7 @@ function Router(){
         <Route path='/seasonalproducelist' component={SeasonalProduceList} />
         <Route path='/marketschedulelist' component={MarketScheduleList} />
         <Route path='/about' component={About} />
+        <Route component={Error404} />
       </Switch>
     </div>
   );
